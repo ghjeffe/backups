162ab0e13afd335ce4afd05ba3004d1410220cc7
ghjeffe <ghjeffe@WD-WI148SA1E04N.us.bank-dns.com>
2016-04-22 12:33:15 -0500

    added utilities package

---
+++ utilities/__init__.py
@@ -0,0 +1,27 @@
+import time
+
+def timer(func, timeout=600, interval=5, run_until=True, verbose=False):
+    '''run func for timeout seconds or until run_until every interval'''
+    def inner(*args, **kwargs):
+        time_start = time.time()
+        time_end = time_start + timeout
+        last_run = ''
+        while time.time() < time_end:
+            try:
+                retval = func(*args, **kwargs)
+                last_run = time.time()
+            except TypeError as e:
+                raise e
+            if retval != run_until:
+                if verbose:
+                    print('{}({}) returns {} @ {}'.format(
+                                                     func.__name__
+                                                     ,(args, kwargs)
+                                                     ,retval
+                                                     ,time.time()
+                                                     )
+                          )
+                time.sleep(interval)
+            else:
+                return (time_start, last_run)
+    return inner
