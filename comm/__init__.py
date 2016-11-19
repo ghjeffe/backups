41183135f92445ec14bef2078eebf96cf1775b80
Gary H Jeffers II <garyjeffersii@gmail.com>
2016-11-19 17:35:54 -0600

    with app_pass

--- comm/__init__.py
+++ comm/__init__.py
@@ -2,17 +2,19 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
+app_pass = 'nzhsifueiffygcit' 
+
 fromaddr = "garyjeffersii@gmail.com"
 toaddr = "garyjeffersii@gmail.com"
 msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = toaddr
 msg['Subject'] = "Backups"
-body = "YOUR MESSAGE HERE"
+body = "swedish horses"
 msg.attach(MIMEText(body, 'plain'))
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(fromaddr, "YOUR PASSWORD")
+server.login(fromaddr, app_pass)
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
 server.quit()
