ceca101b8d4bbade4609ae988864d361be4b6c19
ghjeffe <ghjeffe@wd-wi148sa1e04n>
2016-11-21 13:52:17 -0600

    updated comm/__init__.py

--- comm/__init__.py
+++ comm/__init__.py
@@ -2,19 +2,19 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-app_pass = 'nzhsifueiffygcit' 
+APP_PASS = 'nzhsifueiffygcit' 
 
-from_addr = "garyjeffersii@gmail.com"
-to_addr = "garyjeffersii@gmail.com"
-msg = MIMEMultipart()
-msg['From'] = from_addr
-msg['To'] = to_addr
-msg['Subject'] = "Backups"
-body = "swedish horses"
-msg.attach(MIMEText(body, 'plain'))
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(from_addr, app_pass)
-text = msg.as_string()
-server.sendmail(from_addr, to_addr, text)
-server.quit()
+def send_mail(subj, body):
+    from_addr = "garyjeffersii@gmail.com"
+    to_addr = "garyjeffersii@gmail.com"
+    msg = MIMEMultipart()
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = subj
+    msg.attach(MIMEText(body, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(from_addr, app_pass)
+    text = msg.as_string()
+    server.sendmail(from_addr, to_addr, text)
+    server.quit()
