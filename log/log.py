87df7d3a29124b6a4bc52e555b9b0309c65e25ae
ghjeffe <ghjeffe@wd-wi148sa1e04n>
2016-12-06 17:05:56 -0600

    fixed create table statements in log.log.py

--- log/log.py
+++ log/log.py
@@ -1,20 +1,21 @@
-create table "hosts"
+create table hosts
 (
-   "host_id" integer primary key not null autoincrement
-    ,"hostname" text unique
-    ,"last_known_ip" text
-    ,"mac_addr" text check
+   host_id integer primary key autoincrement not null
+    ,hostname text unique
+    ,last_known_ip text
+    ,mac_addr text check
         (
-            length("mac_addr")  == 20
+            length(mac_addr)  == 12
         )
 )
 
-create table "exec"
+create table exec
 (
-   "exec_id" integer primary key not null autoincrement
-   ,"host_id" integer not null 
-   ,"start_stamp" text 
-   ,"end_stamp" text 
-   ,"args" text 
+   exec_id integer primary key autoincrement not null
+   ,host_id integer not null
+   ,start_stamp text
+   ,end_stamp text
+   ,args text
+   ,foreign key(host_id) references hosts(host_id)
 )
-create unique index "exec_host_idx"  on exec(exec_id, host_id) 
+create unique index exec_host_idx on exec(exec_id, host_id) 
